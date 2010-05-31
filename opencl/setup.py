93477c7a71c7c5d3e9e4919b3022a2cfd2e7e2f3
gene <gene@tengentoppa.(none)>
2010-05-31 10:40:19 -0700

    initCL theoretically working

--- opencl/setup.py
+++ opencl/setup.py
@@ -4,7 +4,7 @@ module1 = Extension('opencl_interface',
                     define_macros = [('MAJOR_VERSION', '1'),
                                      ('MINOR_VERSION', '0')],
                     include_dirs = ['/home/gene/epimorphism/opencl'],
-                    libraries = ['GL', 'GLU','X11','Xmu','OpenCL'],
+                    libraries = ['GL', 'GLU','X11','Xmu','OpenCL', 'glut'],
                     sources = ['opencl.cpp'])
 
 setup (name = 'opencl_interface',
